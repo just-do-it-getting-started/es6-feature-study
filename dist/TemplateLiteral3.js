35bd4bf7c59a4f88db8492a1fa226a6adc018d11
KimJiTae <kimshinelove@gmail.com>
2015-11-12 11:59:44 +0900

    TemplateLiteral

---
+++ dist/TemplateLiteral3.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var _templateObject = _taggedTemplateLiteral(["foo\n", "bar"], ["foo\\n", "bar"]);
+
+function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
+
+function quux(strings) {
+	strings[0] === "foo\n";
+	strings[1] === "bar";
+	strings.raw[0] === "foo\\n";
+	strings.raw[1] === "bar";
+	arguments[1] === 42;
+}
+quux(_templateObject, 42);
+
+String.raw(_templateObject, 42) === "foo\\n42bar";
